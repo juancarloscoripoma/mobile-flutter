ff7c72baa821c2052ba11ae3d6f69e5de822bb93
Carlos <jcori.transoft@gmail.com>
2020-03-13 18:23:15 -0400

    added gesture

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutterapp/ui/containerApp.dart';
+import 'package:flutterapp/ui/gestureApp.dart';
 import 'package:flutterapp/ui/materialAppHome.dart';
 
 void main() => runApp(
   MaterialApp(
     title: "Flutter Tutorial",
-    home: MaterialAppHome(),/*AppScaffold()*/
+    home: GestureApp(),/*AppScaffold(), MaterialAppHome()*/
   )
 );
